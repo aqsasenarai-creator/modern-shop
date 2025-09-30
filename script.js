83e804e20ba114add518279a88af36b378dda7a2
aqsasenarai-creator <aqsasenarai@gmail.com>
2025-09-30 20:18:22 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+window.addEventListener("load", () => {
+  const loader = document.getElementById("loader");
+  loader.style.opacity = "0";
+  setTimeout(() => loader.style.display = "none", 1000);
+});
+
+gsap.from(".animate-text", {
+  y: 50,
+  opacity: 0,
+  duration: 1,
+  delay: 1
+});
+
+gsap.from(".animate-text-delay", {
+  y: 50,
+  opacity: 0,
+  duration: 1,
+  delay: 1.5
+});
 
